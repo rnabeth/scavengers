9964bc045c9b51de2330f7898fb196729e48c540
Rafael Nabeth <rafaelnabeth@gmail.com>
2015-11-14 22:41:44 +0000

    Loader script

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour
+{
+    public GameObject gameManager;
+
+    public void Awake()
+    {
+        if (GameManager.instance == null)
+            Instantiate(gameManager);
+    }
+}
