a7fc65738652999ce28f6c04a3c550553a24b8c9
Rafael Nabeth <rafaelnabeth@gmail.com>
2015-11-14 22:40:39 +0000

    Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null; //To make the class singleton
+    public BoardManager boardScript;
 
-	// Use this for initialization
-	void Start () {
+    private int level = 4;
+
+	void Awake ()
+	{
+	    if (instance == null)
+	        instance = this;
+        else if (instance != this)
+            Destroy(gameObject); //So we don't accidentaly end up with 2 instances of GameManager
+
+	    DontDestroyOnLoad(gameObject); //To keep score between scenes
+	    boardScript = GetComponent<BoardManager>();
+	    InitGame();
+	}
+
+    private void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
+    // Update is called once per frame
+    /*
+    void Update ()
+    {
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    */
 }
