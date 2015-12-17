9b0a06c9902c44de5f6f3ac4d84e5bab467d853d
Rafael Nabeth <rafaelnabeth@gmail.com>
2015-12-17 23:38:16 +0000

    Enemy script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Player : MovingObject
+{
+    public int wallDamage = 1;
+    public int pointsPerFood = 10;
+    public int pointsPerSoda = 20;
+    public float restartLevelDelay = 1f;
+
+    private Animator animator;
+    private int food;
+    
+    // Use this for initialization
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        //So the player can manage the food score during the level and then store it in the GameManager as we change levels
+        food = GameManager.instance.playerFoodPoints;
+
+        base.Start();
+    }
+
+    private void OnDisable()
+    {
+        //To store food points as we change levels
+        GameManager.instance.playerFoodPoints = food;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!GameManager.instance.playersTurn)
+            return;
+
+        int horizontal = 0;
+        int vertical = 0;
+
+        horizontal = (int) Input.GetAxisRaw("Horizontal");
+        vertical = (int) Input.GetAxisRaw("Vertical");
+
+        //To prevent player from moving diagonally
+        if (horizontal != 0)
+            vertical = 0;
+
+        if (horizontal != 0 || vertical != 0)
+            AttemptMove<Wall>(horizontal, vertical);
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        food--;
+
+        base.AttemptMove<T>(xDir, yDir);
+
+        RaycastHit2D hit;
+        CheckIfGameOver();
+        GameManager.instance.playersTurn = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Exit")
+        {
+            Invoke("Restart", restartLevelDelay);
+            enabled = false;
+        }
+        else if (other.tag == "Food")
+        {
+            food += pointsPerFood;
+            other.gameObject.SetActive(false);
+        }
+        else if (other.tag == "Soda")
+        {
+            food += pointsPerSoda;
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Wall hitWall = component as Wall;
+        hitWall.DamageWall(wallDamage);
+        animator.SetTrigger("playerChop");
+    }
+
+    private void Restart()
+    {
+        //Application.LoadLevel(Application.loadedLevel);
+        SceneManager.LoadScene("Main");
+    }
+
+    public void LoseFood(int loss)
+    {
+        animator.SetTrigger("playerHit");
+        food -= loss;
+        CheckIfGameOver();
+    }
+
+    private void CheckIfGameOver()
+    {
+        if (food <= 0)
+            GameManager.instance.GameOver();
+    }
+}
